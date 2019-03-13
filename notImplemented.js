0285c2a7bb9fd2eecbe3ebd47d72ebcfc60f111c
Lcfvs <michael.rouges@gmail.com>
2019-03-14 00:30:34 +0100

    Undo add context to notImplemented()

--- notImplemented.js
+++ notImplemented.js
@@ -1,5 +1,5 @@
 import log from 'anticore-apis/console/log'
-import bind from 'anticore-core/function/bind'
+import curry from 'anticore-core/function/curry'
 
 function callback (name, log) {
   log('Not implemented : %s', name)
@@ -7,6 +7,6 @@ function callback (name, log) {
   return this
 }
 
-export default function notImplemented (context, name, logger) {
-  return bind(callback, context, name, logger || log)
+export default function notImplemented (name, logger) {
+  return curry(callback, name, logger || log)
 }
